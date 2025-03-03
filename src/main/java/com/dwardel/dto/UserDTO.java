e70a094363bfd105f6d4f54f8e89aa72a2bb2b6d
varshini39 <ragavs95@gmail.com>
2025-03-03 20:38:13 +0530

    fixed issue in userdto

--- src/main/java/com/dwardel/dto/UserDTO.java
+++ src/main/java/com/dwardel/dto/UserDTO.java
@@ -1,16 +1,19 @@
 package com.dwardel.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
     private Long id;
     private String email;
     private String address;
 
     public UserDTO(Long id, String email) {
-    }
-
-    public UserDTO(Long id, String email, String address) {
+        this.id = id;
+        this.email = email;
     }
 }
